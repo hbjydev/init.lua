618d7daa11b85e8c2f8f65118b15fc041ccb1fd0
Hayden Young <hayden@hbjy.dev>
2022-12-21 18:20:52 +0000

    feat: update cmp mappings
    Signed-off-by: Hayden Young <hayden@hbjy.dev>

--- after/plugin/lspzero.lua
+++ after/plugin/lspzero.lua
@@ -31,8 +31,8 @@ lsp.ensure_installed {
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings {
-	["<C-n>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-p>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 	["<C-y>"] = cmp.mapping.confirm({ select = true }),
 	["<C-Space>"] = cmp.mapping.complete(),
 }
