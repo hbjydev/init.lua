3c8daa397fa43f54fc4150c6e34f965f8868ab41
Hayden Young <hayden@hbjy.dev>
2022-12-24 14:49:06 +0000

    feat: update lualine config
    Signed-off-by: Hayden Young <hayden@hbjy.dev>

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,15 +1,25 @@
-require("catppuccin").setup {
-	flavour = "macchiato"
-}
+local lualine = require('lualine')
 
 function ColorMyPencils(color)
 	color = color or "oxocarbon"
+    lualine_settings = {}
 
     if color == "oxocarbon" then
         vim.opt.background = "dark"
+    elseif color == "catppuccin" then
+        vim.opt.background = "dark"
+        lualine_settings {
+            options = {
+                theme = 'catppuccin'
+            }
+        }
+        require("catppuccin").setup {
+            flavour = "macchiato"
+        }
     end
 
 	vim.cmd.colorscheme(color)
+    lualine.setup(lualine_settings)
 end
 
 ColorMyPencils()
