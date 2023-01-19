7f77c43067dca6369aae417b7fa744fdb47f54c4
Hayden Young <hayden@hbjy.dev>
2023-01-19 22:02:30 +0000

    feat: mason-nvim-dap

--- lua/modules/tools/debugger/init.lua
+++ lua/modules/tools/debugger/init.lua
@@ -2,32 +2,50 @@ local hvim = require('core.macros')
 
 hvim.pack {
     "rcarriga/nvim-dap",
+
     lazy = true,
 
     dependencies = {
-        {
-            "rcarriga/nvim-dap-ui",
-            lazy = true,
-            opts = {
-                icons = {
-                    expanded = "",
-                    collapsed = "",
-                    current_frame = ""
-                },
-                controls = {
-                    icons = {
-                        pause = "",
-                        play = "契",
-                        step_into = "",
-                        step_over = "",
-                        step_out = "",
-                        step_back = "",
-                        run_last = "",
-                        terminate = "",
-                    },
-                },
-                floating = { border = "single" },
-            },
-        },
+        {'jay-babu/mason-nvim-dap.nvim'},
+
+        {"rcarriga/nvim-dap-ui"},
     },
+
+    config = function ()
+        local mnd = require('mason-nvim-dap')
+        mnd.setup {
+            ensure_installed = {
+                'bash-debug-adapter',
+                'chrome-debug-adapter',
+                'debugpy',
+                'go-debug-adapter',
+                'js-debug-adapter',
+                'netcoredbg',
+                'php-debug-adapter',
+            },
+
+            automatic_setup = true,
+        }
+
+        require('dapui').setup {
+            icons = {
+                expanded = "",
+                collapsed = "",
+                current_frame = ""
+            },
+            controls = {
+                icons = {
+                    pause = "",
+                    play = "契",
+                    step_into = "",
+                    step_over = "",
+                    step_out = "",
+                    step_back = "",
+                    run_last = "",
+                    terminate = "",
+                },
+            },
+            floating = { border = "single" },
+        }
+    end
 }
