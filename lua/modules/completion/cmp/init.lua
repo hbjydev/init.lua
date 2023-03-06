a00a32f374274a25e504c42ae4d0ea480f0dc870
Hayden Young <hayden@hbjy.dev>
2023-03-06 13:13:14 +0000

    feat: copilot cmp source (forgot this in last commit)

--- lua/modules/completion/cmp/init.lua
+++ lua/modules/completion/cmp/init.lua
@@ -34,6 +34,7 @@ hvim.pack({
 
         local cmp_sources = {
             { name = "neorg", group_index = 1 },
+            { name = "copilot", group_index = 1 },
             { name = "nvim_lsp", group_index = 1 },
             { name = "nvim_lsp_signature_help", group_index = 1 },
             { name = "buffer", group_index = 3 },
